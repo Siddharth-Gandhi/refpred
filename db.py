cca5d89684d9d23a247080eba172e5b122f29d57
Siddharth Gandhi <ssgandhi602@gmail.com>
2023-02-28 01:30:41 +0530

    Refactoring done

--- db.py
+++ db.py
@@ -1,28 +1,51 @@
+"""
+Handles the MongoDB database and it's operations.
+"""
+
+import logging
+import logging.config
 from dataclasses import dataclass, field
+from typing import Any
 
 from pymongo import MongoClient
 from pymongo.collection import Collection
 from pymongo.database import Database
 
+logging.config.fileConfig(fname="logging.conf", disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+
 
 @dataclass(frozen=True)
-class MongoDB:
+class MongoDBClient:
     """A MongoDB database"""
 
     mongo_url: str = "mongodb://localhost:27017"
     db_name: str = "refpred"
     collection_name: str = "test"
-    client: MongoClient = field(init=False, repr=False)
-    db: Database[Any] = field(init=False, repr=False)
-    collection: Collection[Any] = field(init=False, repr=False)
+    client: MongoClient = field(init=False)
+    db: Database[Any] = field(init=False)
+    collection: Collection[Any] = field(init=False)
+    stored: int = field(init=False, default=0)
 
     def __post_init__(self) -> None:
         """Initialize the MongoDB database and collection"""
-        self.client = MongoClient(self.mongo_url)
-        self.db = self.client[self.db_name]
+        # self.client = MongoClient(self.mongo_url)
+        object.__setattr__(self, "client", MongoClient(self.mongo_url))
+        # self.db = self.client[self.db_name]
+        object.__setattr__(self, "db", self.client[self.db_name])
         all_collections = self.db.list_collection_names()
         if self.collection_name in all_collections:
             logger.warning(f"Dropped pre-existing '{self.collection_name}' collection")
             self.db.drop_collection(self.collection_name)
         logger.info(f"Created '{self.collection_name}' collection")
-        self.collection = self.db[self.collection_name]
+        # self.collection = self.db[self.collection_name]
+        object.__setattr__(self, "collection", self.db[self.collection_name])
+
+    def insert_one(self, document: dict) -> None:
+        """Insert a document into the collection"""
+        self.collection.insert_one(document)
+        # self.stored += 1
+        object.__setattr__(self, "stored", self.stored + 1)
+        if self.stored % 100 == 0:
+            logger.info(f"Inserted {self.stored} documents")
+        
---
+++ utils.py
@@ -0,0 +1,15 @@
+'''
+Contains various utility functions.
+'''
+
+def get_reference_url(paper_id, is_arxiv: bool = False) -> str:
+        """Get the URL for the references of a paper"""
+        if is_arxiv:
+            return f"https://api.semanticscholar.org/graph/v1/paper/arXiv:{paper_id}/references?fields=title,abstract,url,venue,publicationVenue,year,referenceCount,citationCount,influentialCitationCount,isOpenAccess,openAccessPdf,authors,externalIds,fieldsOfStudy,s2FieldsOfStudy,publicationTypes,publicationDate,journal,citationStyles"
+        return f"https://api.semanticscholar.org/graph/v1/paper/{paper_id}/references?fields=title,abstract,url,venue,publicationVenue,year,referenceCount,citationCount,influentialCitationCount,isOpenAccess,openAccessPdf,authors,externalIds,fieldsOfStudy,s2FieldsOfStudy,publicationTypes,publicationDate,journal,citationStyles"
+
+def get_paper_url(paper_id, is_arxiv: bool = False) -> str:
+        """Get the URL for a paper"""
+        if is_arxiv:
+            return f"https://api.semanticscholar.org/graph/v1/paper/arXiv:{paper_id}?fields=title,abstract,url,venue,publicationVenue,year,referenceCount,citationCount,influentialCitationCount,isOpenAccess,openAccessPdf,authors,externalIds,fieldsOfStudy,s2FieldsOfStudy,publicationTypes,publicationDate,journal,citationStyles"
+        return f"https://api.semanticscholar.org/graph/v1/paper/{paper_id}?fields=title,abstract,url,venue,publicationVenue,year,referenceCount,citationCount,influentialCitationCount,isOpenAccess,openAccessPdf,authors,externalIds,fieldsOfStudy,s2FieldsOfStudy,publicationTypes,publicationDate,journal,citationStyles"
